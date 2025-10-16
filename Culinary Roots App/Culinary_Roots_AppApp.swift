0ea87728f14caa89955b53dd8d131e4af8069185
Boity <bsialumba@gmail.com>
2025-10-16 12:07:10 +0200

    Initial Commit

---
+++ Culinary Roots App/Culinary_Roots_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culinary_Roots_AppApp.swift
+//  Culinary Roots App
+//
+//  Created by Boitumelo Sialumba on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Culinary_Roots_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
